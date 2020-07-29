b18327cc4554033ece146b2061a1cd6cdc698f49
fmpimentelx <fmpimentelx@gmail.com>
2020-07-29 12:52:16 +0100

    User page

--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -9,34 +10,33 @@ import { ActivatedRoute } from '@angular/router';
 export class UserPage implements OnInit {
 
   username : any; 
-id : any; 
+  role : any; 
+  name : any; 
+  id : any; 
+  midName: any;
+  surname: any;
+  phone: any;
+  ppsn: any;
+  vehicles: any[];
   
-  constructor( private actRoute : ActivatedRoute) { }
+  constructor( private actRoute : ActivatedRoute, private http: HttpClient) { }
   
   ngOnInit() {
-    let id = this.actRoute.snapshot.paramMap.get('username');
-    alert(id)
-
-  	this.actRoute.params.subscribe((data: any) =>{
-  		this.id = data.id;
-  		this.username = data.username;
-  		console.log(data);
-  	});
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    let uri =encodeURI('http://localhost:8080/garage/findById?id=');
+    this.http.get(uri+this.id).subscribe(data => {
+    this.username = data.username;
+    this.role = data.role;
+    this.name = data.customer.name;
+    this.midName = data.customer.midName;
+    this.surname = data.customer.surname;
+    this.phone = data.customer.phone;
+    this.ppsn = data.customer.ppsn;
+    this.vehicles = data.customer.vehicles;
+    console.log(this.vehicles)
+    console.log(data)
+    // this.router.navigate(['user/' +this.data.id]);
+})
   }
-
-  // ngOnInit() {
-  //   this.actRoute.params.subscribe((data: any) =>{
-  // 		this.username = data.username;
-  // 		this.password = data.password;
-  // 		console.log(data);
-  // 	});
- 
-  //   this.sub = this.route.queryParams.subscribe(params => {
-  //     // Defaults to 0 if no query param provided.
-  //     console.log(params['username']+"aa")
-  //     // console.log(params)
-  //     this.username = +params['data'] || null;
-  //   });
-  // }
 
 }
