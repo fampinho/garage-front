7488d7bcdfe38d2b65806a95f3b2a2d524b25b06
fmpimentelx <fmpimentelx@gmail.com>
2020-08-09 22:25:47 +0100

    Routes Update
    Add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,29 +6,61 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
+  // {
+  //   path: 'tabs',
+  //   loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
+  // {
+  //   path: 'tabs/:id',
+  //   loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then(m => m.UserPageModule)
+  },
+  {
+    path: 'user/:id',
+    loadChildren: () => import('./pages/user/user.module').then(m => m.UserPageModule)
   },
   {
     path: 'user-register',
     loadChildren: () => import('./pages/user-register/user-register.module').then(m => m.UserRegisterPageModule)
   },
   {
-    path: 'tabs/:id',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
     path: 'user-update/:id',
-    loadChildren: () => import('./pages/user-update/user-update.module').then( m => m.UserUpdatePageModule)
+    loadChildren: () => import('./pages/user-update/user-update.module').then(m => m.UserUpdatePageModule)
   },
-  // {
-  //   path: 'user',
-  //   loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
-  // },
   {
-    path: 'user/:id',
-    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
+    path: 'staff',
+    loadChildren: () => import('./pages/staff/staff.module').then( m => m.StaffPageModule)
+  },
+  {
+    path: 'staff/:id',
+    loadChildren: () => import('./pages/staff/staff.module').then( m => m.StaffPageModule)
+  },
+  {
+    path: 'staff-update/:id',
+    loadChildren: () => import('./pages/staff-update/staff-update.module').then(m => m.StaffUpdatePageModule)
+  },
+  {
+    path: 'booking',
+    loadChildren: () => import('./pages/booking/booking.module').then( m => m.BookingPageModule)
+  },
+  {
+    path: 'booking/:id',
+    loadChildren: () => import('./pages/booking/booking.module').then( m => m.BookingPageModule)
+  },
+  {
+    path: 'booking-view',
+    loadChildren: () => import('./pages/booking-view/booking-view.module').then( m => m.BookingViewPageModule)
+  },
+  {
+    path: 'booking-view/:id',
+    loadChildren: () => import('./pages/booking-view/booking-view.module').then( m => m.BookingViewPageModule)
+  },
+  {
+    path: 'booking-view/:appointment/:id/:type/:maker/:model/:fuel/:manufacture/:licenceNumber/:name/:midName/:surname/:phone/:email/:ppsn',
+    loadChildren: () => import('./pages/booking-view/booking-view.module').then( m => m.BookingViewPageModule)
   },
 
   // {
